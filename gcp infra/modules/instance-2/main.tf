cc7a6057178b58543a429dfff798a25796d47e76
Bruno Feliano <bruno.feliano@liqi.com.br>
2022-12-27 09:45:45 -0300

    included gcp infra and updated aws-modules directory

---
+++ gcp infra/modules/instance-2/main.tf
@@ -0,0 +1,29 @@
+## Author: Bruno Feliano
+## Date: 26th December 2022
+## Purpose: VM Instance 2
+## Terraform documentation: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance
+
+
+resource "google_compute_instance" "instance2" {
+  name         = "instance-2"
+  machine_type = "f1-micro"
+  zone         = "us-east1-b"
+
+  tags = ["ssh", "http"]
+
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-7"
+    }
+  }
+
+  network_interface {
+    network    = var.vpc
+    subnetwork = var.subnet1
+
+    access_config {
+      // Ephemeral public IP
+    }
+
+  }
+}
